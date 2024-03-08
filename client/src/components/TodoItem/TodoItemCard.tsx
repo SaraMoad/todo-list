cfe8ca884445ceb2550e46d42b710921fbd32b02
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2024-03-08 19:15:43 +1100

    todoitem list and item styling

--- client/src/components/TodoItem/TodoItemCard.tsx
+++ client/src/components/TodoItem/TodoItemCard.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import Button from "../Button/Button";
 import { TodoItem } from "../../services/todoItems";
 import { faTrash, faPencil } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { priorityValue } from "../../services/priorities";
+import styles from "./todoItem.module.scss";
 interface TodoItemProps {
   name: string;
   description: string;
@@ -13,6 +13,7 @@ interface TodoItemProps {
   setTodoItems: (res: any) => any;
   setTodoItem: (res: any) => any;
   setIsOpen: (res: any) => any;
+  dispatchToast: (message: string, variant: string) => unknown;
 }
 
 const TodoItemCard = ({
@@ -24,15 +25,18 @@ const TodoItemCard = ({
   setTodoItems,
   setTodoItem,
   setIsOpen,
+  dispatchToast,
 }: TodoItemProps) => {
   const [checked, setChecked] = useState(false);
-
   const handleClick = () => {
     checked ? setChecked(false) : setChecked(true);
   };
 
   const handleDelete = () => {
-    TodoItem.delete(id).then((res) => setTodoItems(res));
+    TodoItem.delete(id)
+      .then((res) => setTodoItems(res))
+      .catch((res) => dispatchToast(res.message, "error"));
+    dispatchToast("Todo Task Deleted", "success");
   };
 
   const handleEdit = async () => {
@@ -41,18 +45,30 @@ const TodoItemCard = ({
   };
 
   return (
-    <div>
-      <input onClick={handleClick} type="checkbox"></input>
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>{priorityValue[Number(priority)]}</p>
-      <p>Due Date: {dueDate.slice(0, 10)}</p>
-      <Button handleClick={handleEdit}>
-        <FontAwesomeIcon icon={faPencil} />
-      </Button>
-      <Button handleClick={handleDelete}>
-        <FontAwesomeIcon icon={faTrash} />
-      </Button>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.header__item}>
+          <input
+            className={styles.check}
+            onClick={handleClick}
+            type="checkbox"
+          ></input>
+          <h3 className={styles.name}>{name}</h3>
+          <p className={styles.dueDate}>Due Date: {dueDate.slice(0, 10)}</p>
+        </div>
+        <div className={styles.header__item}>
+          <p className={styles.priority}>{priorityValue[Number(priority)]}</p>
+          <button className={styles.button} onClick={handleEdit}>
+            <FontAwesomeIcon className={styles.icon} icon={faPencil} />
+          </button>
+          <button className={styles.button} onClick={handleDelete}>
+            <FontAwesomeIcon className={styles.icon} icon={faTrash} />
+          </button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
   );
 };
--- client/src/containers/TodoList/TodoList.tsx
+++ client/src/containers/TodoList/TodoList.tsx
@@ -1,44 +1,43 @@
-import { useState } from "react";
 import TodoItemCard from "../../components/TodoItem/TodoItemCard";
 import { TodoItem } from "../../services/todoItems";
-import Modal from "../Modal/Modal";
+import styles from "./TodoList.module.scss";
 
 interface TodoListProps {
   todoItems: TodoItem[] | null;
   setTodoItems: (res: any) => any;
+  setTodoItem: (res: any) => any;
+  setIsOpen: (value: boolean) => unknown;
+  dispatchToast: (message: string, variant: string) => unknown;
 }
-const TodoList = ({ todoItems, setTodoItems }: TodoListProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [todoItem, setTodoItem] = useState<TodoItem | undefined>(undefined);
+const TodoList = ({
+  todoItems,
+  setTodoItems,
+  setTodoItem,
+  setIsOpen,
+  dispatchToast,
+}: TodoListProps) => {
   return (
-    <>
+    <div className={styles.container}>
       {todoItems &&
         todoItems?.map((item: TodoItem) => {
           return (
-            <TodoItemCard
-              key={item.id}
-              name={item.name}
-              description={item.description}
-              priority={item.priority}
-              dueDate={item.dueDate}
-              id={item.id}
-              setTodoItems={setTodoItems}
-              setIsOpen={setIsOpen}
-              setTodoItem={setTodoItem}
-            />
+            <div className={styles.card__container}>
+              <TodoItemCard
+                key={item.id}
+                name={item.name}
+                description={item.description}
+                priority={item.priority}
+                dueDate={item.dueDate}
+                id={item.id}
+                setTodoItems={setTodoItems}
+                setIsOpen={setIsOpen}
+                setTodoItem={setTodoItem}
+                dispatchToast={dispatchToast}
+              />
+            </div>
           );
         })}
-
-      {isOpen && (
-        <Modal
-          todoItem={todoItem}
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-          setTodoItem={setTodoItem}
-          setTodoItems={setTodoItems}
-        />
-      )}
-    </>
+    </div>
   );
 };
 
