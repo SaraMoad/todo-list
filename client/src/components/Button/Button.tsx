44b60ef4516129439da3d731bdeae1ad31cf39a2
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2024-03-08 19:15:09 +1100

    button component

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,10 +1,15 @@
+import styles from "./Button.module.scss";
 interface ButtonProps {
   children: React.ReactNode;
   handleClick: () => any;
 }
 
 const Button = ({ handleClick, children }: ButtonProps) => {
-  return <button onClick={handleClick}>{children}</button>;
+  return (
+    <button className={styles.button} onClick={handleClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
